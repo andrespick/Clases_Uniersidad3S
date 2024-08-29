ba0c40e932cffd63edd918c7c66018e45a37b364
Andres Felipe Vela <andresfelipevelaflorez@gmail.com>
2024-08-28 19:50:53 -0500

    se agrega ejercicio mainsout

---
+++ clase1/MainSout.java
@@ -0,0 +1,12 @@
+public class MainSout{
+    /*
+     *     public static void main(String[] args){
+        ejercicio pruebaSout = new ejercicio(1300606, (float) 0.15);
+        float salarioAjustado = pruebaSout.calcularSalario();
+        System.out.println("el salario con ajuste es: "+salarioAjustado);
+
+    }
+     */
+
+        
+}
