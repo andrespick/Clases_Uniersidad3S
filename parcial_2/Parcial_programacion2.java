86b88e22333d0e72451a9c6d9f6a8a1324db658e
andrespick <andresfelipevelaflorez@gmail.com>
2024-10-08 20:34:36 -0500

    se agrega parcial2

--- parcial_2/Parcial_programacion2.java
+++ parcial_2/Parcial_programacion2.java
@@ -9,9 +9,9 @@ package nn.parcial_programacion2;
  */
 public class Parcial_programacion2 {
 
-    // Clase Planeta
+
     public static class Planeta {
-        // Atributos
+
         String nombre;
         int cantidadSatelites;
         double masa; // en kg
@@ -20,7 +20,7 @@ public class Parcial_programacion2 {
         double distanciaAlSol; // en km
         boolean esObservable;
 
-        // Constructor
+
         public Planeta(String nombre, int cantidadSatelites, double masa, double volumen, double diametro, double distanciaAlSol, boolean esObservable) {
             this.nombre = nombre;
             this.cantidadSatelites = cantidadSatelites;
@@ -31,7 +31,6 @@ public class Parcial_programacion2 {
             this.esObservable = esObservable;
         }
 
-        // Métodos para imprimir los valores de los atributos
         public void imprimirAtributos() {
             System.out.println("Nombre: " + nombre);
             System.out.println("Cantidad de Satélites: " + cantidadSatelites);
@@ -42,19 +41,16 @@ public class Parcial_programacion2 {
             System.out.println("Es observable: " + esObservable);
         }
 
-        // Método para calcular la densidad
         public double calcularDensidad() {
             return masa / volumen; // Densidad en kg/m^3
         }
     }
 
-    // Método main, el punto de entrada del programa
+
     public static void main(String[] args) {
-        // Crear objetos Planeta con los nuevos parámetros
         Planeta planeta1 = new Planeta("Tierra", 1, 5.9736E24, 1.08321E12, 12742, 150000000, true);
         Planeta planeta2 = new Planeta("Júpiter", 1, 1.899E27, 1.4313E15, 139820, 750000000, true);
 
-        // Imprimir atributos de los planetas
         System.out.println("Planeta 1:");
         planeta1.imprimirAtributos();
         System.out.println("Densidad: " + planeta1.calcularDensidad() + " kg/m^3\n");
